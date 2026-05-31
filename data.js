62f4978314b2934b09c261616a5abf1000318ac7
Mike-Schvedov <MikeSchvedovMS@Gmail.com>
2026-05-31 10:36:24 +0300

    First Commit

---
+++ data.js
@@ -0,0 +1,30 @@
+const characters = [
+  // Hiragana
+  { char: "あ", type: "hiragana" },
+  { char: "い", type: "hiragana" },
+  { char: "う", type: "hiragana" },
+  { char: "え", type: "hiragana" },
+  { char: "お", type: "hiragana" },
+  { char: "か", type: "hiragana" },
+  { char: "き", type: "hiragana" },
+  { char: "く", type: "hiragana" },
+  { char: "け", type: "hiragana" },
+  { char: "こ", type: "hiragana" },
+  // ... add more
+
+  // Katakana
+  { char: "ア", type: "katakana" },
+  { char: "イ", type: "katakana" },
+  { char: "ウ", type: "katakana" },
+  { char: "エ", type: "katakana" },
+  { char: "オ", type: "katakana" },
+  // ... add more
+
+  // Kanji
+  { char: "日", type: "kanji" },
+  { char: "月", type: "kanji" },
+  { char: "火", type: "kanji" },
+  { char: "水", type: "kanji" },
+  { char: "木", type: "kanji" },
+  // ... add more
+];
